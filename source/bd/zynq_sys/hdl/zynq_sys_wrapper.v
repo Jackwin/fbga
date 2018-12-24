6e74a4751fd80700542cba6628d58ac0c13e0a35
Jackwin <rcwcj@126.com>
2018-12-24 19:26:16 +0800

    1. Add SD, USB, SPI Flash support in the bd 2. Create fsbl and device_tree under the sdk 3. Can boot from SD card

--- source/bd/zynq_sys/hdl/zynq_sys_wrapper.v
+++ source/bd/zynq_sys/hdl/zynq_sys_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
-//Date        : Sat Nov  3 10:07:45 2018
-//Host        : chunjiew-MOBL running 64-bit major release  (build 9200)
+//Date        : Mon Dec 24 17:18:39 2018
+//Host        : DESKTOP-4U66LTL running 64-bit major release  (build 9200)
 //Command     : generate_target zynq_sys_wrapper.bd
 //Design      : zynq_sys_wrapper
 //Purpose     : IP block netlist
